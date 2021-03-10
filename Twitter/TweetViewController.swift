33cf741782205b74a3e20d92a49a17bfcd14de7d
khudaykulovamir <khudaykulovamir@gmail.com>
2021-03-11 02:20:41 +0500

    part 2

---
+++ Twitter/TweetViewController.swift
@@ -0,0 +1,46 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by Amir on 10/03/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController {
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+   
+    @IBOutlet weak var tweetTextView: UITextView!
+    @IBAction func Tweet(_ sender: Any) {
+        if (!tweetTextView.text.isEmpty){
+            TwitterAPICaller.client?.postTweet(tweetString: tweetTextView.text, success:{ self.dismiss(animated: true, completion: nil)}, failure: {(error) in
+                print("error posting tweet  \(error)")
+                self.dismiss(animated: true, completion: nil)
+            })
+        }else {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tweetTextView.becomeFirstResponder()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Twitter/tweetCellTableViewCell.swift
+++ Twitter/tweetCellTableViewCell.swift
@@ -10,7 +10,53 @@ import UIKit
 
 class tweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var favButton: UIButton!
     @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var retweetButton: UIButton!
+    @IBAction func retweet(_ sender: Any) {
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (error) in
+            print("Error is retweeting")
+        })
+    }
+    func setRetweeted(_ isRetweeted:Bool){
+        if (isRetweeted){
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        } else {
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
+    }
+    @IBAction func Favorite(_ sender: Any) {
+        let toBeFavorited = !favorited
+        if (toBeFavorited){
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true)
+            }, failure: { (error) in
+                print("Favorite did not succeed: \(error)")
+            })
+        }else {
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+            }, failure: { (error) in
+                print("Unfavorite did not succeed: \(error)")
+            })
+        }
+    }
+    var favorited:Bool = false
+    var tweetId:Int = -1
+    func setFavorite (_ isFavorited:Bool){
+        favorited = isFavorited
+        if(favorited){
+            favButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+        }
+        else {
+            favButton.setImage(UIImage(named: "favor-icon"), for: UIControl.State.normal)
+        }
+    }
+    
     
     @IBOutlet weak var tweetContent: UILabel!
     override func awakeFromNib() {
